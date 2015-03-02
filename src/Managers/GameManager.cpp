0eed116d15a83ac1c403bb6ae9420027970a6f45
Archomeda <archomeda@hotmail.com>
2015-03-03 00:01:50 +0100

    Add initializer and uninitializer for the game manager

--- src/Managers/GameManager.cpp
+++ src/Managers/GameManager.cpp
@@ -4,6 +4,9 @@
 
 #include "GameManager.h"
 
+// Project includes
+#include "../Games/GameGw2.h"
+
 
 using namespace std;
 using namespace lightfx::games;
@@ -12,11 +15,27 @@ namespace lightfx {
     namespace managers {
 
         size_t GameManager::InitializeGames() {
-            return 0;
+            size_t i = 0;
+
+            auto guildWars2 = make_shared<GameGw2>();
+            if (guildWars2->Initialize()) {
+                ++i;
+                this->AddChild(L"Guild Wars 2", guildWars2);
+            }
+            
+            return i;
         }
 
         size_t GameManager::UninitializeGames() {
-            return 0;
+            size_t i = 0;
+
+            for (size_t j = 0; j < this->GetChildrenCount(); ++j) {
+                if (this->GetChildByIndex(j)->Release()) {
+                    ++i;
+                }
+            }
+
+            return i;
         }
 
     }
