1bf77e120a2abbcc38ecc2f6f13e26238606dacb
Archomeda <archomeda@hotmail.com>
2015-03-08 20:04:28 +0100

    Fix multiple postfixes on version after first build in matrix

--- appveyor/UpdateBuildVersion.ps1
+++ appveyor/UpdateBuildVersion.ps1
@@ -2,18 +2,25 @@ $file = "src/VersionInfo.h"
 
 $version = $env:APPVEYOR_BUILD_VERSION
 if ($env:APPVEYOR_REPO_TAG -eq "false") {
-    $version = "$version-dev"
+    $postfix = "dev"
     if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
-        $version = "$version-PR$($env:APPVEYOR_PULL_REQUEST_NUMBER)"
+        $postfix = "$postfix-PR$($env:APPVEYOR_PULL_REQUEST_NUMBER)"
     } elseif ($env:APPVEYOR_REPO_BRANCH -ne "master") {
-        $version = "$version-$($env:APPVEYOR_REPO_BRANCH)"
+        $postfix = "$postfix-$($env:APPVEYOR_REPO_BRANCH)"
+    }
+
+    if (!($version -like "*-$postfix")) {
+        $version = "$version-$postfix"
     }
 }
 
-Write-Host "Current build version: $version" -ForegroundColor "Yellow"
+Write-Host "AppVeyor build version: $($env:APPVEYOR_BUILD_VERSION)" -ForegroundColor "Yellow"
+Write-Host "Custom build version: $version" -ForegroundColor "Yellow"
 
-Write-Host "  - Apply to $file" -ForegroundColor "Yellow"
-(gc $file) -replace "#define CURRENT_VERSION "".+""","#define CURRENT_VERSION ""$version""" | Set-Content $file
+if ($version -ne $env:APPVEYOR_BUILD_VERSION) {
+    Write-Host "  - Apply to $file" -ForegroundColor "Yellow"
+    (gc $file) -replace "#define CURRENT_VERSION "".+""","#define CURRENT_VERSION ""$version""" | Set-Content $file
 
-Write-Host "  - Send to AppVeyor Build Worker API" -ForegroundColor "Yellow"
-Update-AppveyorBuild -Version $version
+    Write-Host "  - Send to AppVeyor Build Worker API" -ForegroundColor "Yellow"
+    Update-AppveyorBuild -Version $version
+}
