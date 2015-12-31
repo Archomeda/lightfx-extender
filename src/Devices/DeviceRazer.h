5ca33f0b80df1d5ad44e9e685e58a6c5bec71b42
Archomeda <archomeda@hotmail.com>
2015-12-31 04:19:56 +0100

    Move RzChroma library logic to a separate proxy class

--- src/Devices/DeviceRazer.h
+++ src/Devices/DeviceRazer.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// Windows includes
-#include "../Common/Windows.h"
-#include <Windows.h>
-
 // 3rd party includes
 #include "RzChromaSDKDefines.h"
 #include "RzChromaSDKTypes.h"
@@ -11,6 +7,7 @@
 
 // Project includes
 #include "Device.h"
+#include "Proxies/RzChromaSDKProxy.h"
 
 // API exports
 #include "../Common/ApiExports.h"
@@ -25,6 +22,8 @@ namespace lightfx {
             void SetHardware(const bool keyboard, const bool mouse, const bool headset, const bool mousepad, const bool keypad);
 
             virtual bool Initialize() override;
+            virtual bool Release() override;
+
             virtual bool Enable() override;
             virtual bool Disable() override;
 
@@ -35,6 +34,8 @@ namespace lightfx {
             virtual bool PushColorToDevice(const std::vector<timelines::LightColor>& colors) override;
 
         private:
+            std::unique_ptr<proxies::RzChromaSDKProxy> library;
+
             bool useWithKeyboard = true;
             bool useWithMouse = true;
             bool useWithHeadset = true;
