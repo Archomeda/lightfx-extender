37b132f4cdd1bdc16b08b2a3e2185f05df8f10ec
Archomeda <archomeda@hotmail.com>
2015-12-31 16:20:06 +0100

    Fix environment variable and add an informational message

--- appveyor/Test.ps1
+++ appveyor/Test.ps1
@@ -1,3 +1,5 @@
 if ($env:PLATFORM -eq "x86" -or $env:PLATFORM -eq "x64") {
-    vstest.console /Platform:$(env:PLATFORM) /logger:Appveyor bin\Release_Tests\$(env:PLATFORM)\UnitTests.dll
+    vstest.console /Platform:$env:PLATFORM /logger:Appveyor bin\Release_Tests\$env:PLATFORM\UnitTests.dll
+} elseif ($env:PLATFORM -eq "release") {
+    Write-Host "This job is in release mode; skip testing" -ForegroundColor "Yellow"
 }
